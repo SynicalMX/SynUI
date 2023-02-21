150acf60c8810db69cc504dff6adde138ebfbad2
Synical <70176240+SynicalMX@users.noreply.github.com>
2023-02-21 13:25:27 -0500

    v0.1 drawing api

--- src/lib/drawing.lua
+++ src/lib/drawing.lua
@@ -1,8 +1,20 @@
 local drawing = {}
 
+local termX, termY = term.getSize()
+
 --- Draws window header
 --- @param title string App title
 --- @param pallete Pallete Header pallete
 function drawing:drawHeader(title, pallete)
-    
+    paintutils.drawLine(1, 1, termX, 1, pallete.primary)
+
+    term.setCursorPos(termX, 1)
+    term.setTextColor(pallete.text)
+    term.write("X")
+
+    local x = (math.floor(termX / 2) - math.floor(title:len() / 2))
+    term.setCursorPos(x, 1)
+    term.write(title)
 end
+
+return drawing
