15c8270b25cdf460642ca7060f1081a11a4f65a9
SynicalMX <70176240+SynicalMX@users.noreply.github.com>
2023-02-22 14:58:20 +0000

    fix text on example

--- examples/basic.lua
+++ examples/basic.lua
@@ -5,7 +5,7 @@ local pallete = {
     ["secondary"] = colors.white,
     ["text"] = colors.white
 }
-
 local app = synui.app:create("Basic", pallete)
 app:draw()
+app.window.setTextColor(colors.black)
 app.window.write("Hello, world!")
